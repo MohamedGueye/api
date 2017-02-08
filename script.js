e6b6c3e31e875c7c40ca15188ab4f2a9cb060b7e
mohamedgueye <mgueye2@hudsonhs.com>
2017-02-08 23:05:27 +0000

    weatther-api-updat

--- script.js
+++ script.js
@@ -10,12 +10,30 @@ $(document).ready(function() {
             "&units=imperial&appid=23a3979215671ded7eb87905805bbffc";
         console.log(city);
         console.log(url);
-        
-        $.get(url, function(data) {
-        $("#temp").append(data.main.temp)
-        $("#hum").append(data.main.humidity)
-        $("#Wind Speed").append(data.wind.speed)
-        });        
+
+           $.get(url, function(data) {
+            $("#temp").html(data.main.temp)
+            $("#hum").html(data.main.humidity)
+            $("#Wind-Speed").html(data.wind.speed)
+            
+            
+            
+            
+            var longitude = data.coord.lon
+            
+            var latitude = data.coord.lat
+            
+            console.log(latitude);
+            console.log(longitude);
+            
+            var mapurl = "https://api.mapbox.com/v4/mapbox.dark/" +
+            longitude+ "," + latitude+ "," +
+            "10/600x600.png?access_token=pk.eyJ1IjoiZG9kZ2VyNDg3IiwiYSI6ImNpeXcxY2xraDAwZHUyd21wam00NWc5NXIifQ.VNP3UdlAUjSJVz3_FrBkEQ"
+            
+            $("img").attr("src", mapurl);
+            console.log(mapurl);
+            
+        });
 
     });
 });
